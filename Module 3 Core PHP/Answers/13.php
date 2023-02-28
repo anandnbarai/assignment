d0d33e249debb6831e23822a84eaeabd165d7c92
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-02-28 17:38:01 +0530

    Update 13.php

--- Module 3 Core PHP/Answers/13.php
+++ Module 3 Core PHP/Answers/13.php
@@ -1,7 +1,8 @@
 <?php
 
 
-/* the array_unique() function only works with simple arrays of scalar values (such as strings, integers, and floats). If your array contains non-scalar values (such as arrays or objects), you may need to use a different approach to remove duplicates. */
+/* the array_unique() function only works with simple arrays of scalar values (such as strings, integers, and floats). 
+If your array contains non-scalar values (such as arrays or objects), you may need to use a different approach to remove duplicates. */
 
 
 $arr = array(1, 2, 3, 2, 4, 3, 5, 6, 5, 6, 4, 3, 2, 4, 3, 5);
