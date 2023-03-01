8f7df871ec73ba21dac19073ffecc9b2ef313b94
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-03-01 06:57:49 +0530

    Update 9.php

--- Module 3 Core PHP/Answers/9.php
+++ Module 3 Core PHP/Answers/9.php
@@ -5,7 +5,9 @@ $b = &$a;
 $b = "2$b";
 
 echo $a."<br>".$b;
-echo "<br><b>Explaination :</b> In first step Value 1 is given to variable a. In second step variable b is assign as a reference to variable a means both variable a and b now references a same memory location and whatever value you assign to any of those references both will point to same value. So In third step 2b is assigned to variable b and now value of b is 21 because here two string concatenates.";
+echo "<br><b>Explaination :</b> In first step Value 1 is given to variable a. In second step variable b is assign as a reference to 
+variable a means both variable a and b now references a same memory location and whatever value you assign to any of those references 
+both will point to same value. So In third step 2b is assigned to variable b and now value of b is 21 because here two string concatenates.";
 
 
 ?>
