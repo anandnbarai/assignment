acc8857be08b56d9ca6cf5f95d7005728934c5e7
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-03-01 06:33:03 +0530

    Add files via upload

--- Module 3 Core PHP/Answers/16.php
+++ Module 3 Core PHP/Answers/16.php
@@ -1,4 +1,15 @@
 <?php
 
+$numbers = array (15, 11, 34, 45, 51) ;
+$count = count($numbers);
+$total = 0;
+
+
+for($i=0; $i < $count ; $i++)
+{
+    $total += $numbers[$i];   //? equivalent to $total = $total + $numbers[$i];
+}
+
+echo "<b>Total of Five element is : </b>".$total;
 
 ?>
