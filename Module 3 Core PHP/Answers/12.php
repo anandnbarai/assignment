7bebdb8e20db8139e1c72f36e0d737dcdc62b01a
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-03-01 10:37:29 +0530

    Add files via upload

--- Module 3 Core PHP/Answers/12.php
+++ Module 3 Core PHP/Answers/12.php
@@ -1,4 +1,11 @@
 <?php
+//* JSON (JavaScript Object Notation) is a lightweight text-based, language-independentf data exchange format. 
 
+//? To convert a JSON data string to a PHP array, you can use the json_decode($json) function. The json_decode() function accepts 
+//? the JSON string as the first parameter and a few additional parameters to control the process of converting JSON to a PHP array. 
+
+
+$json = '{"UK": "London", "India": "New Delhi", "IT": "Rome"}';
+print_r (json_decode($json));
 
 ?>
