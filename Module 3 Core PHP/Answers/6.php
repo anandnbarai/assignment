237c1c195eb3edf7b60894806bf1d8bb2c62a834
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-03-02 10:22:45 +0530

    Add files via upload

--- Module 3 Core PHP/Answers/6.php
+++ Module 3 Core PHP/Answers/6.php
@@ -6,6 +6,9 @@
 //* For example, 153 is an Armstrong number because it is a 3-digit number and
 //* the sum of the cubes of its digits (1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153) is equal to the number itself.
 
+$num = 153;
+$x = $num;
 
+while($x)
 
 ?>
